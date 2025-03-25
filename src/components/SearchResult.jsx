6d73c6bb87ee31718032e1a8d2b24969afbe89a0
Hamza Idris <hamza.idris41@hotmail.com>
2025-03-25 16:42:22 -0500

    Added Search Bar to Landing Page and Checkout Page that searches the Database

--- src/components/SearchResult.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-import "./SearchResult.css";
-
-export const SearchResult = ({ result, onClick }) => {
-    return <div className="search" onClick={onClick}>{result.name}</div>;
-};
